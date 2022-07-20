447965407e55b7d299c6363a7b7c49e8d2ab0960
Simon Bear <simon.bear@csiro.au>
2022-07-21 04:01:34 +1000

    fixup

--- src/test/groovy/test521/TestServiceSpec.groovy
+++ src/test/groovy/test521/TestServiceSpec.groovy
@@ -1,9 +1,9 @@
 package test521
 
+import grails.test.hibernate.HibernateSpec
 import grails.testing.services.ServiceUnitTest
-import spock.lang.Specification
 
-class TestServiceSpec extends Specification implements ServiceUnitTest<TestService>{
+class TestServiceSpec extends HibernateSpec implements ServiceUnitTest<TestService> {
 
     def setup() {
     }
@@ -11,8 +11,33 @@ class TestServiceSpec extends Specification implements ServiceUnitTest<TestServi
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "test sort"() {
+        setup:
+        def testOne = new TestOne(name: 'name', testEmbed: new TestEmbed(code: 'code', value: 'value')).save(failOnError: true)
+        def testTwo = new TestTwo(desc: 'desc', testEmbed: new TestEmbed(code: 'code', value: 'value'), testOne: testOne).save(failOnError: true)
+        when:
+        def results = service.checkSortAssociatedProperty()
+        then:
+        results.size() == 1
+    }
+
+    void "test sortEmbed"() {
+        setup:
+        def testOne = new TestOne(name: 'name', testEmbed: new TestEmbed(code: 'code', value: 'value')).save(failOnError: true)
+        def testTwo = new TestTwo(desc: 'desc', testEmbed: new TestEmbed(code: 'code', value: 'value'), testOne: testOne).save(failOnError: true)
+        when:
+        def results = service.checkSortEmbedProperty()
+        then:
+        results.size() == 1
+    }
+
+    void "test sortAssociatedEmbed"() {
+        setup:
+        def testOne = new TestOne(name: 'name', testEmbed: new TestEmbed(code: 'code', value: 'value')).save(failOnError: true)
+        def testTwo = new TestTwo(desc: 'desc', testEmbed: new TestEmbed(code: 'code', value: 'value'), testOne: testOne).save(failOnError: true)
+        when:
+        def results = service.checkSortAssociatedEmbedProperty()
+        then:
+        results.size() == 1
     }
 }
